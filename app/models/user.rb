66fbaad18e8a105b4fe3daa9ae02306dd8f3be10
aidarkhan <aidar22222222@gmail.com>
2015-07-21 10:10:17 +0000

    lalala

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+    vaildates :name, presence: true, length: {minimum: 2, maximum: 50}
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    validates :password, presence: true, lenght: {minimum: 6}
+    has_secure_password
+end
